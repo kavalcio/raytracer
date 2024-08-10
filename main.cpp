38211bf67b137cdb46c2b60e70da6f9a82fd0309
kavalcio <sarpkavalcioglu@gmail.com>
2024-08-10 13:08:54 -0400

    Create color util

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "vec3.h"
+#include "color.h"
 
 int main() {
 
@@ -16,17 +17,8 @@ int main() {
   for (int j = 0; j < image_height; j++) {
     std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
     for (int i = 0; i < image_width; i++) {
-      auto r = double(i) / (image_width-1);
-      auto g = double(j) / (image_height-1);
-      auto b = 0.0;
-
-      vec3 my_color = vec3(r, g, b);
-
-      int ir = int(255.999 * my_color.r());
-      int ig = int(255.999 * my_color.g());
-      int ib = int(255.999 * my_color.b());
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      color pixel_color = color(double(i) / (image_width-1), double(j) / (image_height-1), 0.0);
+      write_color(std::cout, pixel_color);
     }
   }
 
