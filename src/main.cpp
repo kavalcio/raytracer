7499e031ef66ac7f9b6db5a266e52267896c9176
kavalcio <sarpkavalcioglu@gmail.com>
2024-08-10 17:58:27 -0400

    Add normal shading

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,30 @@
 color sky_color = color(0.5, 0.7, 1.0);
 color ground_color = color(1.0, 1.0, 1.0);
 
-// Derived from the equation for a sphere, this function determines if a ray intersects a sphere
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+/*
+  Derived from the equation for a sphere and the quadratic formula,
+  this function determines if a ray intersects a sphere.
+*/
+double hit_sphere(const point3& center, double radius, const ray& r) {
   vec3 oc = center - r.origin();
-  auto a = dot(r.direction(), r.direction());
-  auto b = -2.0 * dot(r.direction(), oc);
-  auto c = dot(oc, oc) - radius*radius;
-  auto discriminant = b * b - 4 * a * c;
-  return (discriminant >= 0);
+  auto a = r.direction().length_squared();
+  auto h = dot(r.direction(), oc);
+  auto c = oc.length_squared() - radius * radius;
+  auto discriminant = h * h - a * c;
+  if (discriminant < 0) {
+    return -1.0;
+  }
+  return (h - std::sqrt(discriminant)) / a;
 }
 
 color ray_color(const ray& r) {
-  if (hit_sphere(point3(0, 0, -1), 0.5, r))
-    return color(1, 0, 0);
+  double radius = 0.5;
+  point3 center = point3(0, 0, -1);
+  double t = hit_sphere(center, radius, r);
+  if (t > 0.0) {
+    vec3 normal = (r.at(t) - center) / radius;
+    return 0.5 * color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+  }
 
   vec3 unit_direction = normalize(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
