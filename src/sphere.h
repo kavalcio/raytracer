ab183e8597d8ef00c151869eeec5147cc21a5381
kavalcio <sarpkavalcioglu@gmail.com>
2024-08-10 21:03:11 -0400

    Create hittable list class

--- src/sphere.h
+++ src/sphere.h
@@ -20,21 +20,22 @@ class sphere : public hittable {
 
       auto discriminant = h*h - a*c;
       if (discriminant < 0)
-          return false;
+        return false;
 
       auto sqrtd = std::sqrt(discriminant);
 
       // Find the nearest root that lies in the acceptable range.
       auto root = (h - sqrtd) / a;
       if (root <= ray_tmin || ray_tmax <= root) {
-          root = (h + sqrtd) / a;
-          if (root <= ray_tmin || ray_tmax <= root)
-              return false;
+        root = (h + sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+          return false;
       }
 
       rec.t = root;
       rec.p = r.at(rec.t);
-      rec.normal = (rec.p - center) / radius;
+      vec3 outward_normal = (rec.p - center) / radius;
+      rec.set_face_normal(r, outward_normal);
 
       return true;
     }
