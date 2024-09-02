2a507856bb1bc219d7e1c14646cad03ec7d5436b
kavalcio <sarpkavalcioglu@gmail.com>
2024-09-02 12:33:31 -0400

    Improve shell scripts

--- debug.sh
+++ debug.sh
@@ -1,3 +1,5 @@
+set -e
 cmake -B build/Debug -DCMAKE_BUILD_TYPE=Debug
 cmake --build build/Debug
+mkdir -p output
 ./build/Debug/main > ./output/image_debug.ppm
--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+set -e
 cmake -B build/Release -DCMAKE_BUILD_TYPE=Release
 cmake --build build/Release
+mkdir -p output
 ./build/Release/main > ./output/image_release.ppm
